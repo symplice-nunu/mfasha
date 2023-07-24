e181045f89c065be7196e4c1e536321c9b1a8ca2
Symplice <intwarisymplice@gmail.com>
2023-07-24 20:51:56 +0200

    feat: creating page routes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import Login from "./Pages/Login";
 import NotFound from './Pages/404'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import PageLayout from "./Components/PageLayout";
+import Dashboard from "./Pages/Dashboard";
+import Doctor from "./Pages/Doctor";
+import GeneralDirector from "./Pages/GeneralDirector";
+import Nurse from "./Pages/Nurse";
+import Patient from "./Pages/Patient";
+import Roles from "./Pages/Roles";
+import Users from "./Pages/Users";
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-     <Route element={<PageLayout />}>
-    <Route path="/" element={<Login />} />
-    <Route path="*" element={<NotFound />} />
-    </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route element={<PageLayout />}>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/dashboard" element={<Dashboard /> } />
+          <Route path="/doctor" element={<Doctor /> } />
+          <Route path="/dg" element={<GeneralDirector /> } />
+          <Route path="/nurse" element={<Nurse /> } />
+          <Route path="/patient" element={<Patient /> } />
+          <Route path="/roles" element={<Roles /> } />
+          <Route path="/users" element={<Users /> } />
+        </Route>
+      </Routes>
   </BrowserRouter> 
   );
 }
---
+++ src/Pages/Nurse.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nurse() {
+  return (
+    <div>
+      zxfcdsfsdf
+    </div>
+  )
+}

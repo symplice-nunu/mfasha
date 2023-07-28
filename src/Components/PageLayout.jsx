7c451bddc5418aaef494ded0ddb25dd34f972a18
symplice-nunu <intwarisymplice@gmail.com>
2023-07-28 16:47:57 +0530

    feat: creating navbar

--- src/Components/PageLayout.jsx
+++ src/Components/PageLayout.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Footer from './Sections/Footer'
 import Sidebar from './Sections/Sidebar'
+import Navbar from './Sections/Navbar'
 
 export default function PageLayout() {
   return (
     <>
       <div className='flex gap-2'>
-      <div><Sidebar /></div>
-      <div><Outlet/></div>
+        <div><Sidebar /></div>
+        <div className='w-full'>
+          <div><Navbar /></div>
+          <div><Outlet/></div>
+        </div>
       </div>
       <Footer/>
     </>
--- src/Components/Sections/Sidebar.jsx
+++ src/Components/Sections/Sidebar.jsx
@@ -6,20 +6,20 @@ export default function Sidebar() {
   const location = useLocation();
   const active = location.pathname
   return (
-    <div className=' ml-5'>
+    <div className=' pl-5 bg-[#f0f9ff]'>
       <div className='flex'>
       <div>
         <div className='flex gap-3 my-7'>
-          <div><BsHospitalFill className='text-[30px] text-[#6e61e5]' /></div>
-          <div className='mt-1 font-bold'>Mfasha</div>
+          <div><BsHospitalFill className='text-[30px] text-[#6e62e5]' /></div>
+          <div className='mt-1 font-bold font-[Timenewromans]'>Mfasha</div>
         </div>
         <div className='w-[165px]'>
           {
             SidebarData.map((item) => {
               return(
                 <Link to={item.Path}>
-                  <div className={`${active === item.Path ? "bg-[#6e61e5] text-white" : null} flex gap-1 mb-[1px] hover:text-white py-[8px] px-4 hover:bg-[#6e61e5] rounded`}>
-                    <div className='pt-1'>{item.icon}</div>
+                  <div className={`${active === item.Path ? "bg-[#6e61e5] text-white" : null} flex gap-1 mb-[1px] font-[Timenewromans] hover:text-white py-[8px] px-4 hover:bg-[#6e61e5] rounded`}>
+                    <div className='pt-[2px]'>{item.icon}</div>
                     <div className='text-[14px] '>{item.Name}</div>
                   </div>
                 </Link>
@@ -29,7 +29,7 @@ export default function Sidebar() {
         </div>
       </div>
       <div>
-        <div className='bg-gray-300 w-[1px] h-screen mx-2'></div>
+        <div className='bg-gray-300 w-[1px] h-screen ml-5'></div>
       </div>
       </div>
     </div>
